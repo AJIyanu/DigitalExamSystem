d9571d930a987bf52514ead6bd97a2dbfbb98cee
AJ Iyanu <aderemijoseph@live.com>
2024-11-29 19:30:35 +0100

    [syncing] scheduled exams page

--- src/pages/ScheduledExams.js
+++ src/pages/ScheduledExams.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Button, Card, Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Card, Row, Col, Placeholder } from 'react-bootstrap';
 import UserInfo from '../components/UserInfo';
+import '../global.css';
 
 function ScheduledExams() {
+    const [userScheduledExams, setUserScheduledExams] = useState({ length: 4 });
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {}, []);
     return (
         <div>
             <UserInfo />
             <hr />
-            <Row xs={1} md={4} className="g-3">
-                {Array.from({ length: 4 }).map((_, idx) => (
+            <Row xs={1} md={2} lg={3} xl={4} className="g-3">
+                {Array.from(userScheduledExams).map((_, idx) => (
                     <Col key={idx}>
-                        <SubjectCard
+                        <LoadingCard
                             name="Nursing"
                             duration="30 minutes"
                             instruction="Once exam is started, no quitting... Goodluck!"
@@ -31,8 +36,7 @@ function SubjectCard({ name, instruction, duration, img }) {
                 alt={`${name}-icon`}
                 src={img}
                 variant="top"
-                width="10px"
-                // height="200px"
+                className="card-img"
             />
             <Card.Body>
                 <Card.Header>
@@ -63,4 +67,47 @@ function SubjectCard({ name, instruction, duration, img }) {
     );
 }
 
+function LoadingCard() {
+    return (
+        <Card>
+            <Placeholder
+                as={Card.Img}
+                alt="Container icons created by Iconjam - Flaticon"
+                title="container icons"
+                src="/img/container-truck.png"
+                variant="top"
+                className="card-img"
+                animation="glow"
+            />
+            <Card.Body>
+                <Card.Header>
+                    <Placeholder as={Card.Title} animation="glow">
+                        <Placeholder xs={6} />
+                    </Placeholder>
+                    <Placeholder as={Card.Subtitle} animation="glow">
+                        <Placeholder xs={8} />
+                    </Placeholder>
+                </Card.Header>
+                <Card.Body>
+                    <Placeholder as={Card.Text} animation="glow">
+                        <Placeholder xs={7} /> <Placeholder xs={4} />{' '}
+                        <Placeholder xs={4} /> <Placeholder xs={6} />{' '}
+                        <Placeholder xs={8} />
+                    </Placeholder>
+                </Card.Body>
+            </Card.Body>
+            <Card.Footer className="d-grid">
+                <Placeholder.Button xs={8} />
+            </Card.Footer>
+        </Card>
+    );
+}
+
+// Other Functions - not components
+
+export async function fetchDetails(url) {
+    const res = await fetch(url).catch((err) => console.error(err));
+    if (!res.ok) console.error(`request to ${url} didnt go through`);
+    return await res.json();
+}
 export default ScheduledExams;
